d5b7035c5f934933708ffb44548b7e0b7f7c7d00
metr1ckzu <metr1ckzu@gmail.com>
2017-08-05 02:23:30 +0600

    public html, deleted garbage, added tweet.submit and tweet.public

--- twitterclone_app/views.py
+++ twitterclone_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from twitterclone_app.forms import UserCreateForm, AuthenticateForm, TweetForm
 from twitterclone_app.models import Tweet
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -28,3 +29,59 @@ def index(request, auth_form=None, user_form=None):
         return render(request,
                       'home.html',
                       {'auth_form': auth_form, 'user_form': user_form, })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticateForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            # Success
+            return redirect('/')
+        else:
+            # Failure
+            return index(request, auth_form=form)
+    return redirect('/')
+ 
+ 
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+def signup(request):
+    user_form = UserCreateForm(data=request.POST)
+    if request.method == 'POST':
+        if user_form.is_valid():
+            username = user_form.clean_username()
+            password = user_form.clean_password2()
+            user_form.save()
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        else:
+            return index(request, user_form=user_form)
+    return redirect('/')
+
+
+@login_required
+def submit(request):
+    if request.method == 'POST':
+        tweet_form = TweetFrom(data=request.POST)
+        next_url = request.POST.get('next_url', '/')
+        if tweet_form.is_valid():
+            tweet = tweet_form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect(next_url)
+        else:
+            return public(request, tweet_form)
+    return request('/')    
+
+
+@login_required
+def public(request, tweet_form=None):
+    tweet_form = tweet_form or TweetForm()
+    tweets = Tweet.objects.reserve()[:10]
+    return render(request,
+        'public.html',
+        {'tweet_form': tweet_form, 'next_url:': '/tweets',
+        'tweets': tweets, 'username': request.user.username})
