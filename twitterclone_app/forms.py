08ae6c154f6f48cbc4ec5404589742be62ba2c52
metr1ckzu <metr1ckzu@gmail.com>
2017-08-10 15:43:18 +0600

    fixed login error

--- twitterclone_app/forms.py
+++ twitterclone_app/forms.py
@@ -28,13 +28,6 @@ class UserCreateForm(UserCreationForm):
 class AuthenticateForm(AuthenticationForm):
     username = forms.CharField(widget=forms.widgets.TextInput(attrs={'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password'}))
- 
-    def is_valid(self):
-        form = super(AuthenticateForm, self).is_valid()
-        for f, error in self.errors.iteritems():
-            if f != '__all__':
-                self.fields[f].widget.attrs.update({'class': 'error', 'value': strip_tags(error)})
-        return form
 
 
 class TweetForm(forms.ModelForm):
